eaab155d3124a73a757891d45789d982e44c1b87
Simon Bolduc Beaudoin <simonbolducbeaudoin@gmail.com>
2025-01-30 15:55:49 -0500

    (Update) Test  sandbox update

--- tests/test_autocorr.py
+++ tests/test_autocorr.py
@@ -19,7 +19,7 @@ Period = Fraction(F,R).denominator
 nb_fft = 1
 l_fft = 2048
 delta = 5 # Offset to avoid zero variance
-timesteps = 2**20 # Number of time steps
+timesteps = 2**30# Number of time steps
 
 dt = 1/R  # Time step size
 
@@ -27,17 +27,31 @@ dt = 1/R  # Time step size
 time = np.r_[0:Period*2]*dt
 Nchunk = timesteps//(Period*2)
 time = np.tile(time, Nchunk)
-
 np.random.seed(42)
-data = np.array([
-    int(np.random.normal(0, np.sqrt(abs(1000*np.sin(F*2*np.pi * t)) + delta))) for t in time
-], dtype=np.int16)
+# data = np.array([
+    # int(np.random.normal(0, np.sqrt(abs(1000*np.sin(F*2*np.pi * t)) + delta))) for t in time
+# ], dtype=np.int16)
+
+if len(time) > 2**20:
+    base_length = 2**20
+    base_data = np.array([
+        int(np.random.normal(0, np.sqrt(abs(1000*np.sin(F*2*np.pi * t)) + delta))) 
+        for t in np.linspace(0, time[-1], base_length)
+    ], dtype=np.int16)
+
+    repeats = len(time) // base_length + 1  # Ensure enough repetitions
+    data = np.tile(base_data, repeats)[:len(time)]  # Repeat and truncate
+else:
+    data = np.array([
+        int(np.random.normal(0, np.sqrt(abs(1000*np.sin(F*2*np.pi * t)) + delta))) 
+        for t in time
+    ], dtype=np.int16)
+
 
 acorr_m_1 = autocorr_cyclo(data,F,R,nb_fft  ,l_fft  ,Mmax=-1,norm="forward")
 acorr_m_2 = autocorr_cyclo(data,F,R,nb_fft*2,l_fft  ,Mmax=-1,norm="forward")
 acorr_m_3 = autocorr_cyclo(data,F,R,nb_fft  ,l_fft*2,Mmax=-1,norm="forward")
-acorr_m_4 = autocorr_cyclo_py_Vpy(data,F,R,l_fft  ,Mmax=-1,norm="forward")
-
+acorr_m_4 = autocorr_cyclo_py_Vpy(data,F,R,l_fft    ,Mmax=-1,norm="forward")
 
 # Visualizing the result
 fig,axs = subplots(2,1)
@@ -80,5 +94,19 @@ ms = np.r_[np.r_[:len(acorr_m_1)//2+1] , np.r_[-len(acorr_m_1)//2+1:0]]
 for m,(acorr1,acorr4) in enumerate(zip(acorr_m_1,acorr_m_4)) :
     axs[0].plot(np.fft.fftfreq(l_fft,dt),abs(acorr1),label="{}".format(ms[m]))
     axs[1].plot(np.fft.fftfreq(l_fft,dt),abs(acorr4)                         )
+
+fig.legend() 
+
+# Implementation python vs C++ différence pour m=0 et m=1
+fig,axs = subplots(2,1)
+time_slice= slice(None,1000)
+
+ms = np.r_[np.r_[:len(acorr_m_1)//2+1] , np.r_[-len(acorr_m_1)//2+1:0]]
+for m,(acorr1,acorr4) in enumerate(zip(acorr_m_1,acorr_m_4)) :
+    if m > 1 :
+        break
+    axs[0].plot(np.fft.fftfreq(l_fft,dt),abs(acorr1),label="{}".format(ms[m]))
+    axs[1].plot(np.fft.fftfreq(l_fft,dt),abs(acorr1-acorr4)                         )
+    
 
 fig.legend() 
